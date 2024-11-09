428756e7e089770c3a32b76879e9b1b99abccf10
BranGitfox <vixfgit@gmail.com>
2024-11-09 10:46:36 +0300

    refactor: adding body oveflow & avoiding container overflow-x

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const [activeMenu, setActiveMenu] = useState<boolean>(false)
   return (
 
-    <div className="container sm relative flex justify-center m-auto overflow-x-hidden">
+    <div className="container sm relative flex justify-center m-auto">
 
         <div className='bg-blue-900  w-[100%] absolute container border rounded-xl border-blue-900 shadow-sm top-6 m-auto p-6 flex  justify-between items-center'>
             <div className='flex gap-4'>  
