4610d0d1734d629aff695df767236abaf3720deb
BranGitfox <vixfgit@gmail.com>
2024-11-09 08:43:03 +0300

    feat: stylising footer statt

--- src/App.tsx
+++ src/App.tsx
@@ -50,8 +50,19 @@ function App() {
                 
             </div>
         </div>
-      <div className="absolute top-[650px] md:top-[843px] h-[120px] bg-black w-[100%]">
-          hhh
+      <div className="absolute top-[650px] grid grid-cols-3 bg-gray-900  md:top-[843px] h-[120px] bg-black w-[100%] p-4">
+            <div className="flex flex-col items-center justify-center border-r-2 border-blue-600">
+                    <p className='font-bold text-xl text-gray-300 '>40</p>
+                    <p className='text-white font-light'>Repository</p>
+            </div>
+            <div className="flex flex-col  items-center justify-center border-r-2 border-blue-600">
+                    <p className='font-bold text-xl text-gray-300 '>40</p>
+                    <p className='text-white font-light'>Repository</p>
+            </div>
+            <div className="flex flex-col  items-center justify-center ">
+                    <p className='font-bold text-xl text-gray-300 '>40</p>
+                    <p className='text-white font-light'>Repository</p>
+            </div>
       </div>
     </div>
   )
