50dda29955b2a4d1b158dd53ad1f7dea46ea1ebe
BranGitfox <vixfgit@gmail.com>
2024-11-11 23:24:13 +0300

    refactor: changing number font-size

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -32,16 +32,16 @@ function Stats() {
     <>
             <div className="absolute top-[650px] grid grid-cols-3 bg-gray-900  md:top-[843px] h-[120px]  w-[100%] p-4">
             <div className="flex flex-col items-center justify-center border-r-2 border-blue-600">
-                    <p className='font-bold text-xl text-gray-300 '>{isLoading?'Loading...':data?.public_repos}</p>
+                    <p className='font-bold text-2xl text-gray-300 '>{isLoading?'Loading...':data?.public_repos}+</p>
                     <p className='text-white font-light'>Repository</p>
             </div>
             <div className="flex flex-col  items-center justify-center border-r-2 border-blue-600">
-                    <p className='font-bold text-xl text-gray-300 '>{isLoading?'Loading...':data?.public_repos}</p>
-                    <p className='text-white font-light'>Stars</p>
+                    <p className='font-bold text-2xl text-gray-300 '>{isLoading?'Loading...':data?.followers}+</p>
+                    <p className='text-white font-light'>Followers</p>
             </div>
             <div className="flex flex-col  items-center justify-center ">
-                    <p className='font-bold text-xl text-gray-300 '>{isLoading?'Loading...':data?.public_repos}</p>
-                    <p className='text-white font-light'>Commits</p>
+                    <p className='font-bold text-2xl text-gray-300 '>{isLoading?'Loading...':data?.following}+</p>
+                    <p className='text-white font-light'>Following</p>
             </div>
       </div>
    
