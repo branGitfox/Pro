b303518f23cb8e58efd11f6969400e7d3119d111
BranGitfox <vixfgit@gmail.com>
2024-11-11 23:21:57 +0300

    feat: adding loader before fetching data

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -32,15 +32,15 @@ function Stats() {
     <>
             <div className="absolute top-[650px] grid grid-cols-3 bg-gray-900  md:top-[843px] h-[120px]  w-[100%] p-4">
             <div className="flex flex-col items-center justify-center border-r-2 border-blue-600">
-                    <p className='font-bold text-xl text-gray-300 '></p>
+                    <p className='font-bold text-xl text-gray-300 '>{isLoading?'Loading...':data?.public_repos}</p>
                     <p className='text-white font-light'>Repository</p>
             </div>
             <div className="flex flex-col  items-center justify-center border-r-2 border-blue-600">
-                    <p className='font-bold text-xl text-gray-300 '>{followersCount}</p>
+                    <p className='font-bold text-xl text-gray-300 '>{isLoading?'Loading...':data?.public_repos}</p>
                     <p className='text-white font-light'>Stars</p>
             </div>
             <div className="flex flex-col  items-center justify-center ">
-                    <p className='font-bold text-xl text-gray-300 '>{followingCount}</p>
+                    <p className='font-bold text-xl text-gray-300 '>{isLoading?'Loading...':data?.public_repos}</p>
                     <p className='text-white font-light'>Commits</p>
             </div>
       </div>
