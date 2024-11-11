93e3362b6bc3ccd8d395c0d11fb377e873470c68
BranGitfox <vixfgit@gmail.com>
2024-11-11 23:32:36 +0300

    feat:installing dependencies& adding spiner loader

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -1,4 +1,4 @@
-
+import { CircleLoader } from "react-load-animations-plus";
 import { useEffect, useState } from "react"
 import axios from 'axios'
 interface Github {
@@ -32,15 +32,15 @@ function Stats() {
     <>
             <div className="absolute top-[650px] grid grid-cols-3 bg-gray-900  md:top-[843px] h-[120px]  w-[100%] p-4">
             <div className="flex flex-col items-center justify-center border-r-2 border-blue-600">
-                    <p className='font-bold text-2xl text-gray-300 '>{isLoading?'Loading...':data?.public_repos}+</p>
+                    <p className='font-bold text-2xl text-gray-300 '>{isLoading?( <CircleLoader color="Aqua" />):data?.public_repos}+</p>
                     <p className='text-white font-light'>Repository</p>
             </div>
             <div className="flex flex-col  items-center justify-center border-r-2 border-blue-600">
-                    <p className='font-bold text-2xl text-gray-300 '>{isLoading?'Loading...':data?.followers}+</p>
+                    <p className='font-bold text-2xl text-gray-300 '>{isLoading?( <CircleLoader color="Aqua" />):data?.followers}+</p>
                     <p className='text-white font-light'>Followers</p>
             </div>
             <div className="flex flex-col  items-center justify-center ">
-                    <p className='font-bold text-2xl text-gray-300 '>{isLoading?'Loading...':data?.following}+</p>
+                    <p className='font-bold text-2xl text-gray-300 '>{isLoading?( <CircleLoader color="Aqua" />):data?.following}+</p>
                     <p className='text-white font-light'>Following</p>
             </div>
       </div>
