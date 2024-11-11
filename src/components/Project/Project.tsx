f5ed6064645c4fadddc9a60a9510306dbe781363
BranGitfox <vixfgit@gmail.com>
2024-11-11 15:39:42 +0300

    refacrot: fixing calling github api

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -2,16 +2,53 @@ import { AnimateOnScroll } from "animate-on-scroll-framer"
 import { Settings } from "@mui/icons-material"
 import { FaArrowRight } from "react-icons/fa6"
 import ProjectCard from "./ProjectCard"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { ProjectInfo } from "./ProjectCard"
 import projects from "../../assets/data/project"
+import axios from 'axios'
+interface Github {
+  public_repos:number,
+  followers:number,
+  following:number
 
+}
 function Project() {
     const [project] = useState<ProjectInfo[]>(projects)
+    const [data, setData] = useState<Github>()
+    const [repos, setRepos] = useState<number|undefined>(0)
+    const [followers, setFollowers] = useState<number|undefined>(0)
+    const [following, setFollowing] = useState<number|undefined>(0)
+
+    const getGithubData =  async () => {
+      
+          await axios.get('https://api.github.com/users/BranGitfox')
+          .then(res => setData({public_repos:res.data.public_repos, followers:res.data.followers, following:res.data.following}))
+          .then(() =>setFollowers(data?.followers))
+          .then(() =>setRepos(data?.public_repos))
+          .then(() =>setFollowing(data?.following))
+          .catch(err => console.log(err.message))
+     
+    }
+
+    useEffect(() => {
+        getGithubData()
+
+           setInterval(() => {
+         
+            if(typeof repos !== 'undefined'){
+                for(let i=0; i< repos;i++) {
+                    
+                }
+            }
+      }, 500)
+      }, [])
+      
+   
+  
     
   return (
    <>
-               <div className="absolute top-[2200px] md:top-[1000px] lg:top-[1800px] w-[100%]" >
+               <div className="absolute top-[2200px] md:top-[1800px]  w-[100%]" >
             <AnimateOnScroll duration={4} animation='fadeInUp'>
             <h3 className='text-white font-medium text-center' >My Projects <Settings/></h3>
             <h2 className='text-center text-white text-3xl md:text-4xl font-bold mt-5'>My Recent Projects</h2>
