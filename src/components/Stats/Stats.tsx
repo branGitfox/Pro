6ff6bb98788aca6cdef34e8f34f6e3c2271ccbd2
BranGitfox <vixfgit@gmail.com>
2024-11-11 23:20:26 +0300

    feat: adding normal api call again

--- src/components/Stats/Stats.tsx
+++ src/components/Stats/Stats.tsx
@@ -10,7 +10,7 @@ interface Github {
 function Stats() {
       
         const [data, setData] = useState<Github>()
-
+        const [isLoading, setIsLoading] = useState<boolean>(true)
         
     
     
@@ -18,17 +18,21 @@ function Stats() {
           
               await axios.get('https://api.github.com/users/BranGitfox')
               .then(res => setData({public_repos:res.data.public_repos, followers:res.data.followers, following:res.data.following}))
+              .then(() => setIsLoading(false))
               .catch(err => console.log(err.message))
          
         }
   
+        useEffect(() => {
+              getGithubData()          
+        }, [])
 
  
   return (
     <>
             <div className="absolute top-[650px] grid grid-cols-3 bg-gray-900  md:top-[843px] h-[120px]  w-[100%] p-4">
             <div className="flex flex-col items-center justify-center border-r-2 border-blue-600">
-                    <p className='font-bold text-xl text-gray-300 '>{reposCount}</p>
+                    <p className='font-bold text-xl text-gray-300 '></p>
                     <p className='text-white font-light'>Repository</p>
             </div>
             <div className="flex flex-col  items-center justify-center border-r-2 border-blue-600">
