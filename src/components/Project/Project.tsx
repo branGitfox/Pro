eaa3df2701f39e251c8e35f590ee1a2885ca74ce
BranGitfox <vixfgit@gmail.com>
2024-11-12 18:31:32 +0300

    refactor:correction du responsive

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -11,14 +11,14 @@ function Project() {
     const onlyThree = project.filter((pro, index) => index <= 2 && pro)
   return (
    <>
-               <div className="absolute top-[2400px] md:top-[1800px]  w-[100%]" >
+               <div className="absolute top-[2400px] md:top-[430%] lg:top-[1800px]  w-[100%]" >
             <AnimateOnScroll duration={4} animation='fadeInUp'>
             <h3 className='text-white font-medium text-center' >My Projects <Settings/></h3>
             <h2 className='text-center text-white text-3xl md:text-4xl font-bold mt-5'>My Recent Projects</h2>
             <div className="mt-3 text-center md:w-[600px] m-auto">
                 <p className="text-light  text-white">As a full stack developer, it is more than necessary to create useful, revolutionary, and long-term projects. So here are some excerpts from my recent projects that I am proud of.</p>
             </div>
-            <div className="mt-11 flex flex-wrap w-[100%] gap-y-4 justify-evenly">
+            <div className="mt-11 flex flex-wrap w-[100%] gap-4 justify-evenly">
                {onlyThree.map((pro, index) =>  <ProjectCard key={index} title={pro.title} img={pro.img} link={pro.link} infos={pro.infos}/>)}
             </div>
             </AnimateOnScroll>
