99330c9eab2f9e5a80d25e2b2ef4352ad318da61
BranGitfox <vixfgit@gmail.com>
2025-03-23 20:40:36 +0300

    ref: change timeline info

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -18,34 +18,28 @@ function About() {
 <div className="flex gap-x-3">
  
   <div className="min-w-14 text-end">
-    <span className="text-xs text-gray-500 ">12:05PM</span>
+    <span className="text-xs text-blue-500 text-nowrap">2021-2022</span>
   </div>
 
   <div className="relative last:after:hidden after:absolute after:top-7 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
     <div className="relative z-10 size-7 flex justify-center items-center">
       <div className="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"></div>
     </div>
-  </div>
+  </div>  
 
 
 
   <div className="grow pt-0.5 pb-8">
-    <h3 className="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white">
-      <svg className="shrink-0 size-4 mt-1" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z"></path>
-        <polyline points="14 2 14 8 20 8"></polyline>
-        <line x1="16" x2="8" y1="13" y2="13"></line>
-        <line x1="16" x2="8" y1="17" y2="17"></line>
-        <line x1="10" x2="8" y1="9" y2="9"></line>
-      </svg>
-      Created "Preline in React" task
+    <h3 className="flex gap-x-1.5 font-semibold text-white">
+     
+      Bachelor Serie D - L1 Génie Logiciel
     </h3>
-    <p className="mt-1 text-sm text-gray-600 ">
-      Find more detailed insctructions here.
+    <p className="mt-1 text-sm text-gray-200 ">
+    Obtention du diplome de Bacc serie D puis integré L'insititut Superieur Polytechnique de Madagascar (ISPM) en suivant le parcours Informatique de Gestion, de Genie Logiciel et inteligence Artificiel.
     </p>
-    <button type="button" className="mt-1 -ms-1 p-1 inline-flex items-center gap-x-2 text-xs rounded-lg border border-transparent text-gray-500 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none  dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
+    <button type="button" className="mt-1 -ms-1 p-1 inline-flex items-center gap-x-2 text-xs rounded-lg border border-transparent text-gray-200 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none  ">
       <img className="shrink-0 size-4 rounded-full" src="https://images.unsplash.com/photo-1659482633369-9fe69af50bfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8auto=format&fit=facearea&facepad=3&w=320&h=320&q=80" alt="Avatar" />
-      James Collins
+      Brandon Fidelin
     </button>
   </div>
 
@@ -54,7 +48,7 @@ function About() {
 <div className="flex gap-x-3">
 
   <div className="min-w-14 text-end">
-    <span className="text-xs text-gray-500 ">12:05PM</span>
+    <span className="text-xs text-blue-500 text-nowrap ">2022-2023</span>
   </div>
 
   <div className="relative last:after:hidden after:absolute after:top-7 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
@@ -66,68 +60,48 @@ function About() {
 
 
   <div className="grow pt-0.5 pb-8">
-    <h3 className="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white">
-      Release v5.2.0 quick bug fix 🐞
+    <h3 className="flex gap-x-1.5 font-semibold text-white">
+      L2 Génie Logiciel - Formation en Developpement Web - Stage de 02 mois
     </h3>
-    <button type="button" className="mt-1 -ms-1 p-1 inline-flex items-center gap-x-2 text-xs rounded-lg border border-transparent text-gray-500 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none  dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
-      <span className="flex shrink-0 justify-center items-center size-4 bg-white border border-gray-200 text-[10px] font-semibold uppercase text-gray-600 rounded-full dark:bg-neutral-800 dark:border-neutral-700 ">
-        A
-      </span>
-      Alex Gregarov
-    </button>
-  </div>
-
-</div>
-
-<div className="flex gap-x-3">
-
-  <div className="min-w-14 text-end">
-    <span className="text-xs text-blue-500 ">12:05PM</span>
-  </div>
-
-  <div className="relative last:after:hidden after:absolute after:top-7 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
-    <div className="relative z-10 size-7 flex justify-center items-center">
-      <div className="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"></div>
-    </div>
-  </div>
-
-  <div className="grow pt-0.5 pb-8">
-    <h3 className="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white">
-      Marked "Install Charts" completed
-    </h3>
-    <p className="mt-1 text-sm text-blue-600 ">
-      Finally! You can check it out here.
+    <p className="mt-1 text-sm text-gray-200 ">
+      Monté en niveau L2 de mon parcours puis suivi une formation de developpement web avec Certifiaction chez Digital Training Center (DTC) pendant une durée de 03 mois. Ensuite fait une stage de 02 mois pendant laquel j'ai pu developpé une application web de gestion d'employer et salaire.
     </p>
-    <button type="button" className="mt-1 -ms-1 p-1 inline-flex items-center gap-x-2 text-xs rounded-lg border border-transparent text-gray-500 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none  dark:hover:bg-neutral-700 dark:focus:bg-neutral-700">
+    <button type="button" className="mt-1 -ms-1 p-1 inline-flex items-center gap-x-2 text-xs rounded-lg border border-transparent text-gray-200 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none  ">
+      <img className="shrink-0 size-4 rounded-full" src="https://images.unsplash.com/photo-1659482633369-9fe69af50bfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8auto=format&fit=facearea&facepad=3&w=320&h=320&q=80" alt="Avatar" />
+      Brandon Fidelin
+    </button>
+  </div>
+
+</div>
+
+<div className="flex gap-x-3">
+
+  <div className="min-w-14 text-end">
+    <span className="text-xs text-blue-500 ">2024</span>
+  </div>
+
+  <div className="relative last:after:hidden after:absolute after:top-7 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
+    <div className="relative z-10 size-7 flex justify-center items-center">
+      <div className="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"></div>
+    </div>
+  </div>
+
+  <div className="grow pt-0.5 pb-8">
+    <h3 className="flex gap-x-1.5 font-semibold text-white ">
+      L3 Génie Logiciel - Stage 03 mois
+    </h3>
+    <p className="mt-1 text-sm text-gray-200 ">
+      Monté en niveau L3 de mon parcours, stagiaire au Ministère du Travail, de L'Emplois, et de la FOnction Publique pour une durée de à 03 mois en vue d'obtention du Diplome de Licence.
+    </p>
+    <button type="button" className="mt-1 -ms-1 p-1 inline-flex items-center gap-x-2 text-xs rounded-lg border border-transparent text-gray-200 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none  ">
       <img className="shrink-0 size-4 rounded-full" src="https://images.unsplash.com/photo-1659482633369-9fe69af50bfb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=3&w=320&h=320&q=80" alt="Avatar"/>
-      James Collins
+      Brandon Fidelin
     </button>
   </div>
 
 </div>
 
-<div className="flex gap-x-3">
 
-  <div className="min-w-14 text-end">
-    <span className="text-xs text-blue-500 ">12:05PM</span>
-  </div>
-
-  <div className="relative last:after:hidden after:absolute after:top-7 after:bottom-0 after:start-3.5 after:w-px after:-translate-x-[0.5px] after:bg-gray-200 dark:after:bg-neutral-700">
-    <div className="relative z-10 size-7 flex justify-center items-center">
-      <div className="size-2 rounded-full bg-gray-400 dark:bg-neutral-600"></div>
-    </div>
-  </div>
-
-  <div className="grow pt-0.5 pb-8">
-    <h3 className="flex gap-x-1.5 font-semibold text-gray-800 dark:text-white">
-      Take a break ⛳️
-    </h3>
-    <p className="mt-1 text-sm text-blue-600 ">
-      Just chill for now... 😉
-    </p>
-  </div>
-
-</div>
 
 </div>
 
