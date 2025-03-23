fd9c04dba41f198ea91ad1de4ef65e413b7e40c8
BranGitfox <vixfgit@gmail.com>
2025-03-23 09:21:14 +0300

    style: add margin top to project cards

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -16,9 +16,9 @@ function Project() {
           <p className="mt-1 text-neutral-400">Voici quelques projets que je fait personnel et collaboratif avec lesquel j'ai pu m'expertiser dans mon domaine et resoudre des problemes specifiques.</p>
         </div>
         <div className="max-w-[429px] mx-auto  bg-transparent border-1 border-gray-700 mb-5 h-10 rounded-full flex-col ">
-            <button className="bg-slate-600 text-neutral-300 h-full px-3 rounded-l-full">Application Web</button>
-            <button className="bg-slate-700 text-neutral-300 h-full px-3 ">Site Web</button>
-            <button className="bg-slate-700 text-neutral-300 h-full px-3 rounded-r-full">Outils De Developpement</button>
+            <button className="bg-slate-600 text-neutral-300 h-full px-3 rounded-l-full hover:bg-slate-600">Application Web</button>
+            <button className="bg-slate-700 text-neutral-300 h-full px-3 hover:bg-slate-600">Site Web</button>
+            <button className="bg-slate-700 text-neutral-300 h-full px-3 rounded-r-full hover:bg-slate-600">Outils De Developpement</button>
 
 
            
